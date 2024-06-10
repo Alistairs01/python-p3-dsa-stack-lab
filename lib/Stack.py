b5c2d49488d8abd2eeda7a3d8a76057feb4c5ae3
Alistairs <alistairs.gatheru@student.moringaschool.com>
2024-06-10 16:14:44 +0300

    stack_lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,44 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        if items is None :
+            self.items = []
+        else:
+            self.items = items
+        self.limit = limit
+        
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+
+        else :
+            return None
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            return None
 
     def peek(self):
-        pass
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            raise IndexError("peek from empty stack")
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) >= self.limit
 
     def search(self, target):
-        pass
+        for index, value in enumerate(self.items):
+            if value ==  target:
+                return len(self.items) - (index +1)
+        else:
+            return -1
